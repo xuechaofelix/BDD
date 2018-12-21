b086d901d5001e45e5f47464645d13650f01c103
Xuechao <xuechaofelix@hotmail.com>
2018-12-21 10:15:08 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <ext/hash_map>
 #include <unordered_map>
+#include <google/dense_hash_map>
 #include <string>
 #include <time.h>
 
@@ -74,69 +75,21 @@ int main()
 	list<vector<Satisfy_Result> > * l;
 	clock_t startTime,endTime;
 	startTime = clock();
-	Node * result = N_Queens(12,b);
+	Node * result = N_Queens(8,b);
 	cout<<"Construct BDD Down!"<<endl;
 	l = b->Satisfy_All(result);
 	endTime = clock();
 	cout<< "Totle Time : " <<(double)(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;
 	b->GenerateDotFile("eight_queens.dot",result);
 	cout<<l->size()<<endl;
-	// list<vector<Satisfy_Result> >::iterator iter = l->begin();
-	// for(;iter!=l->end();iter++)
-	// {
-	// 	vector<Satisfy_Result>::iterator iter1 = iter->begin();
-	// 	for(;iter1!=iter->end();iter1++)
-	// 	{
-	// 		cout<<*iter1<<" ";
-	// 	}
-	// 	cout<<endl;
-	// }
-
-	// Node * const_0 = b->Get_0_Const();
-	// Node * const_1 = b->Get_1_Const();
-	// Node * f_1 = b->Var(1,"f_1");
-	// Node * f_2 = b->Var(2,"f_2");
-	// Node * f_3 = b->Var(3,"f_3");
-	// Node * f_4 = b->Var(4,"f_4");
-	// Node * f_5 = b->Var(5,"f_5");
-	// Node * f_6 = b->Var(6,"f_6");
-	// f_1->Show();
-	// f_2->Show();
-	// f_3->Show();
-	// Node * result = b->OR(f_1,f_2,"f_4");
-	// result = b->AND(result,f_3,"f_5");
-	// result = b->NOT(result,"f_6");
-	// result = b->AND(result,b->NOT(f_4));
-	// result = b->AND(result,b->NOT(f_5));
-	// result = b->OR(result,b->NOT(f_6));
-	// //result->Set_high_Node(result->Get_low_Node());
-	// // result = b->Restrict(result,2,false,"f_7");
-	// // result->Show();
-	// vector<Satisfy_Result> * l1;
-	// //result = b->GetNodeByName("f_6");
-	// l1 = b->Satisfy(result);
-	// vector<Satisfy_Result>::iterator iter1 = l1->begin();
-	// for(;iter1!=l1->end();iter1++)
-	// {
-	// 	cout<< *iter1<<" ";
-	// }
-	// cout<<endl;
-	 
-	// //result = b->GetNodeByName("f_6");
-	
-	// cout<<l->size()<<endl;
-	// iter = l->begin();
-	// for(;iter!=l->end();iter++)
-	// {
-	// 	vector<Satisfy_Result>::iterator iter1 = iter->begin();
-	// 	for(;iter1!=iter->end();iter1++)
-	// 	{
-	// 		cout<<*iter1<<" ";
-	// 	}
-	// 	cout<<endl;
-	// }
-	//cout<<*b->Satisfy_All(result)<<endl;
-	//cout<<l;
-	// string fileName = "BDD.dot";
-	// b->GenerateDotFile(fileName,result);
+	list<vector<Satisfy_Result> >::iterator iter = l->begin();
+	for(;iter!=l->end();iter++)
+	{
+		vector<Satisfy_Result>::iterator iter1 = iter->begin();
+		for(;iter1!=iter->end();iter1++)
+		{
+			cout<<*iter1<<" ";
+		}
+		cout<<endl;
+	}
 }
